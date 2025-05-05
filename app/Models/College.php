583c3da83f54bbf2c03fdc065a1ec02bacf44de7
Muzakir-amypo <m.ahamed@amypo.in>
2025-05-05 12:59:56 +0530

    Users and College migrations, sanctum api

---
+++ app/Models/College.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class College extends Model
+{
+    protected $fillable = ['college_name','college_short_name','status','created_by','updated_by'];
+}
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,16 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('dob')->nullable();
+            $table->integer('gender')->nullable();
+            $table->integer('role')->nullable();
+            $table->bigInteger('contact_number')->nullable();
+            $table->string('profile')->nullable();
+            $table->integer('status')->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->json('updated_by')->nullable();
+            $table->dateTime('password_validate_at')->nullable();
+            $table->json('access')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
