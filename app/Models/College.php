5df834b4b4715ccdcbb43ed2b47c5d7778e56135
Muzakir-amypo <m.ahamed@amypo.in>
2025-05-05 14:25:16 +0530

    Update College.php

--- app/Models/College.php
+++ app/Models/College.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class College extends Model
 {
-    protected $fillable = ['college_name','college_short_name','status','created_by','updated_by'];
+    protected $fillable = ['college_name','college_short_name','status','created_by','updated_by','college_image'];
 }
