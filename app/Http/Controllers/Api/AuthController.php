3dfb737bb69d825a073a7310e9c016091c56d077
Karthick-dev17 <karthicknagaraj17@gmail.com>
2025-05-13 15:08:26 +0530

    feat:added my file

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
         $user = User::where('email', $request->email)->first();
 
         if (!$user) {
-            return response()->json(['message' => 'The email does not exist', 'status' => 401], 401);
+            return response()->json(['message' => 'The email does not exist', 'status' => 422,'errors' =>['email'=>['The email does not exist']]], 422);
         }
 
         if (!Hash::check($request->password, $user->password)) {
